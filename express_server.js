c6343304a59a5ee0906936cd4a626a97c7a79028
Marcela Fernandes <mam.fernandes@hotmail.com>
2023-02-20 20:55:31 -0700

    added new route to render urls_show

--- express_server.js
+++ express_server.js
@@ -25,8 +25,13 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
-// Route for urls
+// Create route for urls
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+  });
+
+app.get("/urls/:id", (req, res) => {
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  res.render("urls_show", templateVars);
   });
